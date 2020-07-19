d31cf542b014ea664ab29efc2f0685d86f7140fd
chapparavi22 <chapparavi22@gmail.com>
2020-07-18 21:48:08 -0400

    new files added for mybootapp

--- my.kubectl.pod.definitions/commands.sh
+++ my.kubectl.pod.definitions/commands.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 minikube ssh
+kubectl config get-contexts
 kubectl config set-context --current --namespace=dev
 kubectl create secret docker-registry publicdocker-secret --docker-server=registry.hub.docker.com --docker-username=chapparavi22 --docker-password=rambabu2 --docker-email=chapparavi22@gmail.com
 
