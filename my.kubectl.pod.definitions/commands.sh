4bd938058c68cab31870beb2ca65f084650947dc
ravi.chappa <chapparavi22@gmail.com>
2021-03-28 15:45:38 -0400

    password hashed

--- my.kubectl.pod.definitions/commands.sh
+++ my.kubectl.pod.definitions/commands.sh
@@ -2,5 +2,5 @@
 minikube ssh
 kubectl config get-contexts
 kubectl config set-context --current --namespace=dev
-kubectl create secret docker-registry publicdocker-secret --docker-server=registry.hub.docker.com --docker-username=chapparavi22 --docker-password=rambabu2 --docker-email=chapparavi22@gmail.com
+kubectl create secret docker-registry publicdocker-secret --docker-server=registry.hub.docker.com --docker-username=chapparavi22 --docker-password=****** --docker-email=chapparavi22@gmail.com
 
